8f5b47c015d9dd31beb2b0a940705ce03155cdc1
Bryan Wiseman <bwiseman@sqr6592.local>
2018-03-28 11:54:37 -0700

    Create file if file doesn't exist

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,13 +1,137 @@
 
 
-class Customer
-  attr_accessor :trip, :trip_total, :account
-  attr_reader :name
+class Customers
 
-  def initialize(name, amount=0)
-    @name = name
-    @trip_total = 0
-    @trip = Trip.new
-    @account = Account.new(amount)
+  @@filepath = nil
+  def self.filepath=(path=nil)
+    @@filepath = path
   end
+
+  def self.file_usable?
+    @@filepath && File.exists?(@@filepath)    
+  end
+
+  def self.create_file
+    File.open(@@filepath, 'w') unless file_exists?
+    return file_usable?
+  end
+
+  def self.saved_customers
+    customers = []
+    if file_usable?
+      file = File.new(@@filepath, 'r')
+      file.each_line do |line|
+        customer << Customers.new.import_line(line.chomp)
+      end
+      file.close
+    end
+    return customers
+  end
+
+  def self.build_using_customer_attrabutes
+    args = {}
+    print "Name: "
+    args[:given_name] = gets.chomp.strip
+
+    print "Last Name: "
+    args[:family_name] = gets.chomp.strip
+
+    print "Address: "
+    args[:address_line_1] = gets.chomp.strip
+
+    print "Address: "
+    args[:address_line_2] = gets.chomp.strip
+
+    print "City: "
+    args[:locality] = gets.chomp.strip
+
+    print "State: "
+    args[:administrative_district_level_1] = gets.chomp.strip
+
+    print "Zip Code: "
+    args[:postal_code] = gets.chomp.strip
+
+    print "Country: "
+    args[:country] = gets.chomp.upcase.strip
+
+    print "Phone Number: "
+    args[:phone_number] = gets.chomp.strip
+
+    print "Notes: "
+    args[:note] = gets.chomp.strip
+
+    return self.new(args)
+  end
+
+  def initialize(args = {})
+    @given_name   = args[:given_name]                      || ""
+    @last_name    = args[:family_name]                     || ""
+    @address      = args[:address_line_1]                  || ""
+    @address2     = args[:address_line_2]                  || ""
+    @city         = args[:locality]                        || ""
+    @state        = args[:administrative_district_level_1] || ""
+    @zip          = args[:postal_code]                     || ""
+    @country      = args[:country]                         || ""
+    @phone_number = args[:phone_number]                    || ""
+    @note         = args[:note]                            || ""
+  end
+
+  def import_line(line)
+    line_array = line.split("\t")
+    @given_name, @last_name, @address, @address2, @city, @zip, @country, @phone_number, @note = line_array
+    return self
+  end
+
+  def save
+    return false unless Customers.file_usable?
+    File.open(@@filepath, 'a') do |file|
+      file.puts "#{[@given_name, @last_name, @address, @address2, @city, @zip, @country, @phone_number, @note]}"
+    end
+    return true
+  end
+
 end
+
+
+
+
+
+
+  # def person
+  #   build_using_customer_attrabutes()
+  #   person = {
+  # given_name:  @given_name,
+  # family_name:  @last_name,
+  # email_address:  @email_address,
+  # address: {
+  #   address_line_1: @address,
+  #   address_line_2: @address2,
+  #   locality: @city,
+  #   administrative_district_level_1: @state,
+  #   postal_code: @zip,
+  #   country: @country
+  # },
+  # phone_number: @phone_number,
+  # note: @no
+  # }
+  # end
+
+#takes customer info and writes to file
+
+
+
+
+
+
+
+
+#   attr_accessor :trip, :trip_total, :account
+#   attr_reader :name
+#
+#   def initialize(name, amount=0)
+#     @name = name
+#     @trip_total = 0
+#     @trip = Trip.new
+#     @account = Account.new(amount)
+#   end
+# end
--- lib/main.rb
+++ lib/main.rb
@@ -1,15 +1,84 @@
-require_relative 'lib/travel_company'
-require_relative 'lib/customer'
-require_relative 'lib/locations'
+# require_relative 'lib/travel_company'
+ require_relative 'customer'
+# require_relative 'lib/locations'
+
 
 class Main
-  def self.run
-    panama  = Trip.new('Panama', '3 weeks', 5_000)
-    ascension = Trip.new('Ascension', '3 weeks', 10_000)
-    baja    = Trip.new('Baja', '3 weeks', 3_000)
-    tirps = [panama, ascension, baja]
-    buyer   = Customer.new('anonymous', 50_000_000)
-    spear_adventure = TravelCompany.new(sales, trip, )
-    return buyer, travel_company
+  class Config
+    def self.default_file_path
+      File.absolute_path(".")
+    end
+    @@actions = ['quit']
+    def self.actions; @@actions; end
+  end
+
+
+  def initialize
+    Customers.filepath = Config.default_file_path
+    if Customers.file_usable?
+      puts "Found"
+    elsif Customers.create_file
+      puts "File Created"
+    else
+      puts "Create failed"
+      exit!
+
+    end
+  end
+
+  def loop!
+    result = nil
+    until result == :quit
+      action, args = get_action
+      result = do_action(action, args)
+    end
+    conclusion
+  end
+
+  def do_action(action, args=[])
+    case action
+    when "quit"
+      return :quit
+    else
+      puts "error"
+    end
+  end
+
+  def conclusion
+    puts "\n\nHave a nice trip\n\n\n"
+  end
+
+  def get_action
+    action = nil
+    # Keep asking for user input until we get a valid action
+      until Main::Config.actions.include?(action)
+        puts "Actions: " + Main::Config.actions.join(", ")
+        print "> "
+        user_response = gets.chomp
+        args = user_response.downcase.strip.split(' ')
+        #action =
+        action = user_response.downcase.strip
+        # end
+      return action, args
+    end
   end
 end
+
+
+
+
+guide = Main.new
+guide.loop!
+
+
+
+  #   def self.run
+  #     panama  = Trip.new('Panama', '3 weeks', 5_000)
+  #     ascension = Trip.new('Ascension', '3 weeks', 10_000)
+  #     baja    = Trip.new('Baja', '3 weeks', 3_000)
+  #     tirps = [panama, ascension, baja]
+  #     buyer   = Customer.new('anonymous', 50_000_000)
+  #     spear_adventure = TravelCompany.new(sales, trip, )
+  #     return buyer, travel_company
+  #   end
+  # end
